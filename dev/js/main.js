3e39838b437a8a037f2f23b3aa259ac6842a1372
Adam Brady <soundslikework@gmail.com>
2016-02-25 15:45:20 -0500

    Possible merge conflict??

--- dev/js/main.js
+++ dev/js/main.js
@@ -124,10 +124,94 @@ val[1].forEach(function(val, i) {
 //DO WE LOOP THROUGH THE FINAL RESULTS ARRAY AND PULL OUT MATCHING VALUES BETWEEN 1990-2012? 
 //DO WE CREATE STAGING ARRAYS FOR THE CHART? 8 ARRAYS WITH OBJECTS CONTAINING DATES 1990-2012, AND THEN WE LOOP THROUGH THE RESULTS ARRAY, MATCHING THE DATE AND PLACING THE VALUE? ADAM VOTES FOR THIS SOLUTION BECAUSE THIS MEANS WE ONLY HAVE TO WRITE THE CHART ONCE, AND WE CAN USE 0 VALUES, WHICH MAKES IT EASY IF WE DON'T GET VALUES BACK FROM THE API
 //CHART FUNCTION
+var chartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var chartOptions = {
+
+    ///Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
+
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 1,
+
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+
+    //Boolean - Whether the line is curved between points
+    bezierCurve : true,
+
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension : 0.4,
+
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
+
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 4,
+
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+
+    //String - A legend template
+    // legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+};
+
+var chartSet = function() {
+// Get context with jQuery - using jQuery's .get() method.
+var ctx = $("#myChart").get(0).getContext("2d");
+// var ctx = document.getElementById("myChart");
+var ctxt = ctx.getContext("2d");
+// This will get the first returned node in the jQuery collection.
+var myLineChart = new Chart(ctx).Line(chartData);
+  
+}
 
 //INIT FUNCTION
 app.init = function() {
   app.getData();
+  app.chartSet();
 };
 
 //DOC READY
