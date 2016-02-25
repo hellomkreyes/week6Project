57e42058797f8d0d5f585e794198af8ad7665a48
M.K. Reyes <mkgreyes@gmail.com>
2016-02-24 21:40:48 -0500

    HTML markup, basic sass setup and edits to svg.

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,14 @@
 
 var app = {};
 
+app.responseTest = {};
+
 app.endpointBiogas = 'http://api.worldbank.org/countries/CAN;USA;FRA;DEU;ITA;JPN;RUS;GBR/indicators/3.1.9_BIOGAS.CONSUM';
 app.endpointWind = 'http://api.worldbank.org/countries/CAN;USA;FRA;DEU;ITA;JPN;RUS;GBR/indicators/3.1.5_WIND.CONSUM';
 app.endpointGeo = 'http://api.worldbank.org/countries/CAN;USA;FRA;DEU;ITA;JPN;RUS;GBR/indicators/3.1.7_GEOTHERMAL.CONSUM';
 app.endpointHydro = 'http://api.worldbank.org/countries/CAN;USA;FRA;DEU;ITA;JPN;RUS;GBR/indicators/3.1.3_HYDRO.CONSUM';
 app.endpointSolar = 'http://api.worldbank.org/countries/CAN;USA;FRA;DEU;ITA;JPN;RUS;GBR/indicators/3.1.6_SOLAR.CONSUM';
+
 // Country
 // CAN - Canada
 // USA - USA
@@ -54,68 +57,92 @@ app.getData = function () {
   });
 
   // Wind
-  $.ajax({
-    url: 'http://proxy.hackeryou.com',
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      reqUrl: app.endpointWind,
-      format: 'json'
-    }
-  }).then(function (result) {
-    console.log(result);
-    app.displayData(result);
-  });
+  //  $.ajax( {
+  // 	url: 'http://proxy.hackeryou.com',
+  //  	method: 'GET',
+  //   	dataType: 'json',
+  //   	data: {
+  //    	reqUrl: app.endpointWind,
+  //  		format: 'json'
+  // 	}
+  // }).then(function(result) {
+  // 	console.log(result);
+  // 	app.displayData(result);
+  // });
 
-  // Geothermal
-  $.ajax({
-    url: 'http://proxy.hackeryou.com',
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      reqUrl: app.endpointGeo
-    }
-  }).then(function (result) {
-    console.log(result);
-    app.displayData(result);
-  });
+  // // Geothermal
+  // $.ajax( {
+  // 	url: 'http://proxy.hackeryou.com',
+  //   	method: 'GET',
+  //   	dataType: 'json',
+  //   	data: {
+  //  		reqUrl: app.endpointGeo,
+  //  		format: 'json'
+  // 	}
+  // }).then(function(result) {
+  // 	console.log(result);
+  // 	app.displayData(result);
+  // });
 
-  // Hydro
-  $.ajax({
-    url: 'http://proxy.hackeryou.com',
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      reqUrl: app.endpointHydro
-    }
-  }).then(function (result) {
-    console.log(result);
-    app.displayData(result);
-  });
+  // // Hydro
+  //  $.ajax( {
+  // 	url: 'http://proxy.hackeryou.com',
+  //   	method: 'GET',
+  //   	dataType: 'json',
+  //   	data: {
+  //   		reqUrl: app.endpointHydro,
+  //   		format: 'json'
+  // 	}
+  // }).then(function(result) {
+  //  // console.log(result);
+  // 	app.displayData(result);
+  // });
 
-  // Solar
-  $.ajax({
-    url: 'http://proxy.hackeryou.com',
-    method: 'GET',
-    dataType: 'json',
-    data: {
-      reqUrl: app.endpointSolar
-    }
-  }).then(function (result) {
-    console.log(result);
-    app.displayData(result);
-  });
+  // // Solar
+  //  $.ajax( {
+  // 	url: 'http://proxy.hackeryou.com',
+  //   	method: 'GET',
+  //   	dataType: 'json',
+  //   	data: {
+  //  		reqUrl: app.endpointSolar,
+  //  		format: 'json'
+  // 	}
+  // }).then(function(result) {
+  // 	//console.log(result);
+  // 	app.responseTest = result;
+  // 	app.displayData(result);
+  // });
 };
 
 // Store all of the data in variables
-
-// Filter data
 
 // Run data through chart.js or c3js
 
 // Display data on the page
 
-app.displayData = function (energy) {};
+app.displayData = function (val) {
+
+  var good = [];
+  var results = {
+    canada: [],
+    germany: []
+  };
+
+  // Taking in the entire object and isolating the Array at index 1; looping through each object in the Array and filtering out the null values. Then, push the good values to good.forEach where we split it up based on the country(results)
+
+  val[1].forEach(function (val, i) {
+    if (val.value !== null) {
+      good.push(val);
+    }
+  });
+
+  good.forEach(function (val, i) {
+    var country = val.country.value.toLowerCase();
+    results[country].push(val);
+  });
+
+  console.log(results);
+};
 
 app.init = function () {
   app.getData();
